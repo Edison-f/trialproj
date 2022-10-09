381a730a685fc2b9f16127a10337fafa3f2bef4a
Edison-f <osgoodbacon@gmail.com>
2022-10-08 18:23:30 -0700

    Cleaned up unused variables. Change code to use more functions for readability

--- trial project.ino
+++ trial project.ino
@@ -4,167 +4,135 @@
 
 
 Servo servo;
+const int SERVO_PIN = 6;
+const int SERVO_OFFSET = 15;
 
-const int regSelect = 12, enable = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(regSelect, enable, d4, d5, d6, d7);
+//LCD Pins
+const int REG_SELECT_PIN = 12, ENABLE = 11;
+const int d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 
-const byte numChars = 32;
-char receivedChars[numChars];   // an array to store the received data
+LiquidCrystal lcd(REG_SELECT_PIN, ENABLE, d4, d5, d6, d7);
 
-boolean newData = false;
+const byte num_chars = 32;
+char recieved_chars[num_chars];   // an array to store the received data
 
-int dataNumber = 0;             // new for this version
+boolean new_data = false;
+
+int data_as_num = 0;
 
 const int MPU = 0x68;  // MPU6050 I2C address
-float AccX, AccY, AccZ;
-float GyroX, GyroY, GyroZ;
-float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
+float accel_x, accel_y, accel_z;
+float raw_angle_x, raw_angle_y, raw_angle_z;
+float accel_angle_x, accel_angle_y, angle_x, angle_y, gyroAngleZ;
 float roll, pitch, yaw;
-float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
-float elapsedTime, currentTime, previousTime;
-int c = 0;
-int alt_angle;
+float elapsed_time, curr_time, prev_time;
 
-float zeroed_roll;
-float zeroed_pitch;
-float zeroed_yaw;
+int alt_angle; // Accel Angle
 
-float z_accum = 0;
-
-float* z_tracker[50] = {};
-unsigned long count = 0;
-float z_avg;
-
-float target_angle;
 int user_angle = 90;
 float angle_offset = 0;
 float angle_offset_alt = 0;
 
+int target_angle = 0;
 
-const int SERVO_PIN = 6;
-const float modifier = 9.0 / 32.0;
-
-const int INTERRUPT_PIN = 7;
+// Need to set Serial Monitor to New Line and 9600 baud for input and outout 
 
 void setup() {
   Serial.begin(9600);
+
   Wire.begin();                 // Initialize comunication
   Wire.beginTransmission(MPU);  // Start communication with MPU6050 // MPU=0x68
   Wire.write(0x6B);             // Talk to the register 6B
   Wire.write(0x00);             // Make reset - place a 0 into the 6B register
   Wire.endTransmission(true);   //end the transmission
-  delay(20);
+
+  // Setup servo
   pinMode(SERVO_PIN, OUTPUT);
   servo.attach(SERVO_PIN);
-  analogWrite(10, 225);
-  // analogReference()
-  lcd.begin(16, 2);
-  Serial.println("start");
-}
 
-// B10K
-// 320 pot ticks is approx. horizontal  - 90
-// 100 and 550 are approx. vertical     - 0, 180
+  ProcessGyro();
+  CalculateAngle();
+  ZeroSensors();
+
+  // Setup LCD
+  analogWrite(10, 225);
+  lcd.begin(16, 2);
+  
+  Serial.println("Start");
+}
 
 void loop() {  //0x68
   lcd.clear();
+  
   ProcessInput();
-  // === Read acceleromter data === //
+  ProcessGyro();
+  
+  // alt_angle = map((AccY * 100), -100, 100, 180, 0); // Map acceleration values to degrees
+
+  CalculateAngle();
+
+  
+  servo.write(target_angle - SERVO_OFFSET);
+}
+
+void UpdateLCD() {
+  lcd.setCursor(0, 0);
+  lcd.print(String(target_angle) + " " + angle_offset);
+  lcd.setCursor(12, 0);
+  lcd.print(user_angle);
+  lcd.setCursor(0, 1);
+  lcd.print("p:" + String(pitch) + " y:" + String(yaw));
+}
+
+void ProcessGyro() {
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);  // Start with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU, 6, true);  // Read 6 registers total, each axis value is stored in 2 registers
+  
   //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0;  // X-axis value
-  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0;  // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0;  // Z-axis value
+  accel_x = (Wire.read() << 8 | Wire.read()) / 16384.0;  // X-axis value
+  accel_y = (Wire.read() << 8 | Wire.read()) / 16384.0;  // Y-axis value
+  accel_z = (Wire.read() << 8 | Wire.read()) / 16384.0;  // Z-axis value
+  
   // Calculating Roll and Pitch from the accelerometer data
-  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - 0.58;       // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
-  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) + 1.58;  // AccErrorY ~(-1.58)
+  accel_angle_x = (atan(accel_y / sqrt(pow(accel_x, 2) + pow(accel_z, 2))) * 180 / PI) - 0.58;
+  accel_angle_y = (atan(-1 * accel_x / sqrt(pow(accel_y, 2) + pow(accel_z, 2))) * 180 / PI) + 1.58;
+  
   // === Read gyroscope data === //
-  previousTime = currentTime;                         // Previous time is stored before the actual time read
-  currentTime = millis();                             // Current time actual time read
-  elapsedTime = (currentTime - previousTime) / 1000;  // Divide by 1000 to get seconds
+  prev_time = curr_time;                         // Previous time is stored before the actual time read
+  curr_time = millis();                             // Current time actual time read
+  elapsed_time = (curr_time - prev_time) / 1000;  // Divide by 1000 to get seconds
+
   Wire.beginTransmission(MPU);
   Wire.write(0x43);  // Gyro data first register address 0x43
   Wire.endTransmission(false);
   Wire.requestFrom(MPU, 6, true);                    // Read 4 registers total, each axis value is stored in 2 registers
-  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0;  // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
-  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
-  // Correct the outputs with the calculated error values
-  GyroX = GyroX + 0.56;  // GyroErrorX ~(-0.56)
-  GyroY -= 1.45;
-  GyroZ -= .470;
+  raw_angle_x = (Wire.read() << 8 | Wire.read()) / 131.0;  // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
+  raw_angle_y = (Wire.read() << 8 | Wire.read()) / 131.0;
+  raw_angle_z = (Wire.read() << 8 | Wire.read()) / 131.0;
+  
+  // Correct the outputs with error offset values
+  raw_angle_x += 2.72;
+  raw_angle_y -= 1.45;
+  raw_angle_z -= 0.485;
 
-  /*
-  //calc avg z val
-  z_accum += GyroZ;
-  count++;
-  z_avg = z_accum / count;
-  */
-
-  // GyroZ = GyroZ + 0.79;  // GyroErrorZ ~ (-0.8)
   // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
-  gyroAngleX = gyroAngleX + GyroX * elapsedTime;  // deg/s * s = deg
-  gyroAngleY = gyroAngleY + GyroY * elapsedTime;
-  yaw = yaw + GyroZ * elapsedTime;
-  // Serial.println(yaw);
+  angle_x = angle_x + raw_angle_x * elapsed_time;  // deg/s * s = deg
+  angle_y = angle_y + raw_angle_y * elapsed_time;
+  yaw = yaw + raw_angle_z * elapsed_time;
 
   // Complementary filter - combine acceleromter and gyro angle values
-  roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-  pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
+  roll = 0.96 * angle_x + 0.04 * accel_angle_x;
+  pitch = 0.96 * angle_y + 0.04 * accel_angle_y;
 
+  Serial.println("x: " + String(roll) + " \ty: " + String(pitch) + " \tz: " + String(yaw));
+}
 
-
-  // roll += (millis() / 1000.0);
-  // pitch += (millis() / 10000.0);
-  // yaw -= (millis() / 5000.0);
-
-  // Print the values on the serial monitor
-  // Serial.print(roll);
-  // Serial.print("/");
-  // Serial.print(pitch);
-  // Serial.print("/");
-  // Serial.println(yaw);
-  // Serial.println(String(AccY) + "  " + String(AccX));
-  alt_angle = map((AccY * 100), -100, 100, 180, 0);
-  /*
-  if (Serial.peek() == 'z') {
-    while(Serial.available() > 0) {
-      Serial.read();
-    }
-    if (alt_angle < 90) {
-      angle_offset_alt = 90.0 - alt_angle;
-    } else {
-      angle_offset_alt = (-1.0 * alt_angle) + 90;
-    }
-    if (yaw < 90) {
-      angle_offset = 90.0 - yaw;
-    } else {
-      angle_offset = (-1.0 * yaw) + 90;
-    }
-    Serial.println(String(angle_offset) + ", " + String(angle_offset_alt));
-  }
-
-  int peek = Serial.peek();
-  int last;
-  if (Serial.available() && (peek != 0 && peek != 255 && peek != 10)) {
-    last = user_angle;
-    user_angle = Serial.parseInt(SKIP_WHITESPACE);
-    // user_angle = Serial.parseInt();
-
-    user_angle = (user_angle == 0) ? last : user_angle;
-    lcd.clear();
-    Serial.println(String(peek) + " " + String(user_angle));
-  }
-  */
-
-  // int target_angle = ((alt_angle + angle_offset_alt) + (yaw + angle_offset)) / 2.0;
-  int target_angle = (yaw + angle_offset);
+void CalculateAngle() {
+  target_angle = (yaw + angle_offset);
   
-  target_angle = (target_angle > 185) ? 185 : (target_angle < 25) ? 25
-                                                                  : target_angle;
+  CheckAngle();
 
   if(user_angle < 90) {
     target_angle += (90 - user_angle);
@@ -172,38 +140,23 @@ void loop() {  //0x68
     target_angle -= (user_angle - 90);
   }
 
-  // if (target_angle > user_angle) {
-  //   target_angle -= (target_angle - user_angle);
-  // } else {
-  //   target_angle += (user_angle - target_angle);
-  // }
-  // target_angle -= (90.0 - user_angle);
   target_angle = 90 + (90 - target_angle);
+  
+  CheckAngle();
+}
+
+void CheckAngle() {
   target_angle = (target_angle > 185) ? 185 : (target_angle < 25) ? 25
                                                                   : target_angle;
-
-  lcd.setCursor(0, 0);
-  lcd.print(String(target_angle) + " " + angle_offset);
-  lcd.setCursor(12, 0);
-  lcd.print(user_angle);
-  lcd.setCursor(0, 1);
-  lcd.print("p:" + String(pitch) + " y:" + String(yaw));
-  // servo.write(90);
-  // if (target_angle > 90) {
-    servo.write((target_angle)-15);
-  // } else {
-    // servo.write(target_angle);
-  // }
-  // Serial.read();  // Clear for next char
 }
 
 void ProcessInput() {
-  recvWithEndMarker();
+  RecieveWithEndMarker();
   SetAngle();
   ZeroSensors();
 }
 
-void recvWithEndMarker() {
+void RecieveWithEndMarker() {
     static byte ndx = 0;
     char endMarker = '\n';
     char rc;
@@ -212,31 +165,31 @@ void recvWithEndMarker() {
         rc = Serial.read();
 
         if (rc != endMarker) {
-            receivedChars[ndx] = rc;
+            recieved_chars[ndx] = rc;
             ndx++;
-            if (ndx >= numChars) {
-                ndx = numChars - 1;
+            if (ndx >= num_chars) {
+                ndx = num_chars - 1;
             }
         }
         else {
-            receivedChars[ndx] = '\0'; // terminate the string
+            recieved_chars[ndx] = '\0'; // terminate the string
             ndx = 0;
-            newData = true;
+            new_data = true;
         }
     }
 }
 
 void SetAngle() {
-  if(newData && receivedChars[0] != 'z') {
+  if(new_data && recieved_chars[0] != 'z') {
     user_angle = 0;
-    user_angle = atoi(receivedChars);
-    Serial.println("New angle: " + String(receivedChars));
-    newData = false;
+    user_angle = atoi(recieved_chars);
+    Serial.println("New angle: " + String(recieved_chars));
+    new_data = false;
   }
 }
 
 void ZeroSensors() {
-  if(newData && receivedChars[0] == 'z') {
+  if(new_data && recieved_chars[0] == 'z') {
     if (alt_angle < 90) {
       angle_offset_alt = 90.0 - alt_angle;
     } else {
@@ -248,18 +201,6 @@ void ZeroSensors() {
       angle_offset = (-1.0 * yaw) + 90;
     }
     Serial.println(String(angle_offset) + ", " + String(angle_offset_alt) + " Zeroed");
-    newData = false;
+    new_data = false;
   }
-}
-
-void showNewNumber() {
-    if (newData == true) {
-        dataNumber = 0;             // new for this version
-        dataNumber = atoi(receivedChars);   // new for this version
-        Serial.print("This just in ... ");
-        Serial.println(receivedChars);
-        Serial.print("Data as Number ... ");    // new for this version
-        Serial.println(dataNumber);     // new for this version
-        // newData = false;
-    }
 }
